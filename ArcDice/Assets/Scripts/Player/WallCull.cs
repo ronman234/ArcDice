4ac3b70cbcbdd1097fc97b459b61733ddf7090b2
bioa10 <techguy1997@yahoo.com>
2022-07-17 00:58:25 -0500

    Wall Culling

---
+++ ArcDice/Assets/Scripts/Player/WallCull.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCull : MonoBehaviour
+{
+    public Material xray;
+    private GameObject cameraObj;
+    private GameObject creature;
+    private Vector3 direction;
+    private float distance;
+    private bool isActive = false;
+    private static Dictionary<MeshRenderer, Material[]> affectedMeshes;
+    private static int activeCreatures = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (affectedMeshes is null)
+        {
+            affectedMeshes = new Dictionary<MeshRenderer, Material[]>();
+        }
+
+        cameraObj = Camera.main.gameObject;
+        creature = gameObject;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        direction =  creature.transform.position - cameraObj.transform.position;
+        distance = direction.magnitude;
+        direction = direction.normalized;
+
+        int layer = 8;
+        int layerMask = 1 << layer;
+
+        RaycastHit hit;
+        Ray ray = new Ray(cameraObj.transform.position, direction);
+        if (Physics.Raycast(ray, out hit, distance, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (!isActive)
+            {
+                isActive = true;
+                activeCreatures += 1;
+            }
+
+            Debug.DrawRay(cameraObj.transform.position, direction * distance);
+            Debug.Log($"Hit object: {hit.collider.gameObject.name}");
+
+            MeshRenderer mesh = hit.collider.GetComponent<MeshRenderer>();
+            if (!affectedMeshes.ContainsKey(mesh))
+            {
+                affectedMeshes.Add(mesh, mesh.materials);
+            }
+            mesh.materials = new Material[1] { xray };
+
+            Ray rayTwo = new Ray(hit.point, direction);
+            if (Physics.Raycast(rayTwo, out hit, distance - hit.distance, layerMask, QueryTriggerInteraction.Ignore))
+            {
+                Debug.DrawRay(cameraObj.transform.position, direction * distance);
+                Debug.Log($"Hit object: {hit.collider.gameObject.name}");
+
+                mesh = hit.collider.GetComponent<MeshRenderer>();
+                if (!affectedMeshes.ContainsKey(mesh))
+                {
+                    affectedMeshes.Add(mesh, mesh.materials);
+                }
+                mesh.materials = new Material[1] { xray };
+            }
+        }
+        else
+        {
+            if (isActive)
+            {
+                isActive = false;
+                activeCreatures -= 1;
+            }
+
+            if (activeCreatures <= 0)
+            {
+                foreach (KeyValuePair<MeshRenderer, Material[]> keyValuePair in affectedMeshes)
+                {
+                    if (keyValuePair.Key == null)
+                    {
+                        continue;
+                    }
+
+                    keyValuePair.Key.materials = keyValuePair.Value;
+                }
+
+                affectedMeshes.Clear();
+            }
+        }
+    }
+}
